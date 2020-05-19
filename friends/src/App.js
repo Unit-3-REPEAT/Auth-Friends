65e23aaf724013cdf02536063f04bf281fec31c8
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-05-19 14:24:34 -0500

    Created react app boiler plate, installed dependecies and built a basic form

---
+++ friends/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+
+
+
+class App extends React.Component {
+
+
+  render(){
+     return (
+    <div className="App">
+     This is App.
+        <LoginForm />
+    </div>
+  );
+}}
+ 
+
+export default App;
